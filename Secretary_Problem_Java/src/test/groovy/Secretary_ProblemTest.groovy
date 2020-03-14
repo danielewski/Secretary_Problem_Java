0ef2b201d694797e2a7ede3bcc19bfb466e33cee
Taylor Danielewski <danielewski.taylor@gmail.com>
2020-03-14 00:10:23 -0600

    Added test for GetApplicants

--- Secretary_Problem_Java/src/test/groovy/Secretary_ProblemTest.groovy
+++ Secretary_Problem_Java/src/test/groovy/Secretary_ProblemTest.groovy
@@ -1,7 +1,36 @@
 import spock.lang.Specification
 
 class Secretary_ProblemTest extends Specification {
-    def "AutoSkip correctly returns (applicant number * 1/e) rounded to the nearest integer "() {
+
+    def "GetApplicants correctly returns a list of non-repeating integers"() {
+
+        given:
+        int noApplicants = 0;
+        int smallApplicantNum = 25;
+        int medApplicantNum = 2900
+        int largeApplicantNum = 10000;
+        Secretary_Problem secretaryProblem = new Secretary_Problem()
+
+        when:
+        List<Integer> emptyApplicantList = secretaryProblem.getApplicants(noApplicants)
+        List<Integer> smallApplicantList = secretaryProblem.getApplicants(smallApplicantNum)
+        List<Integer> midApplicantList = secretaryProblem.getApplicants(medApplicantNum)
+        List<Integer> largeApplicantList = secretaryProblem.getApplicants(largeApplicantNum)
+
+        then:
+        emptyApplicantList.isEmpty()
+
+        smallApplicantList.size() == smallApplicantNum
+        smallApplicantList.unique().size() == smallApplicantNum
+
+        midApplicantList.size() == medApplicantNum
+        midApplicantList.unique().size() == medApplicantNum
+
+        largeApplicantList.size() == largeApplicantNum
+        largeApplicantList.unique().size() == largeApplicantNum
+    }
+
+    def "getAutoSkip correctly returns (applicant number * 1/e) rounded to the nearest integer"() {
 
         given:
         int zeroApplicants = 0;
@@ -16,13 +45,13 @@ class Secretary_ProblemTest extends Specification {
         int tenApplicantsResult = secretaryProblem.getAutoSkip(tenApplicants)
         int twentyApplicantsResult = secretaryProblem.getAutoSkip(twentyApplicants)
         int twentyOneApplicantsResult = secretaryProblem.getAutoSkip(twentyOneApplicants)
-        int oneHunderApplicantsResult = secretaryProblem.getAutoSkip(oneHundredApplicants)
+        int oneHundredApplicantsResult = secretaryProblem.getAutoSkip(oneHundredApplicants)
 
         then:
         zeroApplicantsResult == 0
         tenApplicantsResult == 4
         twentyApplicantsResult != 8
         twentyOneApplicantsResult == 8
-        oneHunderApplicantsResult == 37
+        oneHundredApplicantsResult == 37
     }
 }
